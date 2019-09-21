5ebabae7080e6b752c0a7e96c71bc3a6190ed637
Kate Laney <kate.a.laney@gmail.com>
2019-09-21 13:26:50 -0400

    added pseudocode and Firebase

---
+++ assets/javascript/project.js
@@ -0,0 +1,26 @@
+// Firebase configuration.
+var firebaseConfig = {
+    apiKey: "AIzaSyAB73QFYhzi3p2RBcJR3t0RjtXQ0c1i7BI",
+    authDomain: "it-s-a-date-9b312.firebaseapp.com",
+    databaseURL: "https://it-s-a-date-9b312.firebaseio.com",
+    projectId: "it-s-a-date-9b312",
+    storageBucket: "",
+    messagingSenderId: "1085675726711",
+    appId: "1:1085675726711:web:1dead53ca6901487ee6372"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// Determine global variables/arrays.
+
+let zipCode = ""
+let date = ""
+
+// Set document.on function to 
+
+// AJAX call.
+
+
+
+// Gather user input.
